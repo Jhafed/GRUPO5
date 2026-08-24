1b20800686942ff715ae8e5abebdb71e3bed7061
alulab14 <alulab14@V20603>
2026-08-24 15:18:10 -0500

    concatenar añadido (manuel) 2.0

--- GRUPO5/GRUPO5.cpp
+++ GRUPO5/GRUPO5.cpp
@@ -11,6 +11,7 @@ public ref class concatenacionn {
 	int a;
 	int b;
 	void concatenernumeros(int num1, int num2) {
+
 		a = num1;
 		b = num2;
 		Console::WriteLine("La concatenación de los números {0} y {1} es: {2}{3}", a, b, a, b);
