d48fc6b63f230a240a9f271507653d0033e43328
alulab14 <alulab14@V20603>
2026-08-24 14:37:01 -0500

    clases añadidas

--- GRUPO5/manuel.cpp
+++ GRUPO5/manuel.cpp
@@ -4,4 +4,21 @@ using namespace System;
 
 int mi_funcion_manuel() {
 	return 0;
-}
+};
+
+public ref class identificacion_persona {
+	String^ nombre;
+	int codigo;
+};
+
+public ref class producto: public identificacion_persona {
+	System::String^ id_producto;
+	double precio;
+	int cantidad;
+};
+
+ref class boleta: public producto {
+	void Emitir_boleta() {
+		// Implementación de la función para emitir la boleta
+	}
+};
