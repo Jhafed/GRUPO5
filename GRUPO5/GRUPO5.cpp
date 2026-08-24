e5e8b9c86ba4bed9748e6b14b80e56506b6646a6
alulab14 <alulab14@V20603>
2026-08-24 15:16:17 -0500

    creando funcion de concatenar (manuel)

--- GRUPO5/GRUPO5.cpp
+++ GRUPO5/GRUPO5.cpp
@@ -6,3 +6,13 @@ int main(array<System::String ^> ^args)
 {
     return 0;
 }
+
+public ref class concatenacionn {
+	int a;
+	int b;
+	void concatenernumeros(int num1, int num2) {
+		a = num1;
+		b = num2;
+		Console::WriteLine("La concatenación de los números {0} y {1} es: {2}{3}", a, b, a, b);
+	}
+};
