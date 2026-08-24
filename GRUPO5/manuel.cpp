bcbb0411b7ccad3b31edc103c4b401657930c5b8
alulab14 <alulab14@V20603>
2026-08-24 14:46:55 -0500

    agregado el console write para emitir boleta

--- GRUPO5/manuel.cpp
+++ GRUPO5/manuel.cpp
@@ -17,8 +17,9 @@ public ref class producto: public identificacion_persona {
 	int cantidad;
 };
 
-ref class boleta: public producto {
+public ref class boleta: public producto {
 	void Emitir_boleta() {
 		// Implementación de la función para emitir la boleta
+		Console::WriteLine("Boleta emitida para el {3} producto: {0} a {1} al precio de {2}", id_producto, nombre, precio, cantidad);
 	}
 };
